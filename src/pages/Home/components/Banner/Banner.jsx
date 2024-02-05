0c7efe7e9a5d00863d578cbb5687e6413e7cabb1
EligibleDev <eligibledev@gmail.com>
2024-02-05 12:10:00 +0600

    the banner is now responsive

--- src/pages/Home/components/Banner/Banner.jsx
+++ src/pages/Home/components/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import WeatherCard from "../../../../components/WeatherCard/WeatherCard";
 
 const Banner = () => {
     return (
-        <section className="h- min-screen min-h-[800px] relative">
+        <section className="h-screen min-h-[700px] relative">
             <video
                 className="absolute w-full h-full object-cover"
                 src="https://finflow.uicore.co/financial-planning/wp-content/uploads/sites/7/2023/03/financial-planning-video.mp4"
@@ -12,9 +12,9 @@ const Banner = () => {
                 playsInline
             ></video>
             <div className="banner-gradient w-full h-full flex items-center z-10 absolute">
-                <div className="max-w-screen-xl mx-auto ">
-                    <div className="w-1/2 space-y-10">
-                        <h1 className="text-6xl font-semibold">
+                <div className="max-w-screen-xl mx-auto px-8 xl:px-0">
+                    <div className="w-full sm:w-2/3 xl:w-1/2 space-y-10">
+                        <h1 className="text-[40px] leading-none sm:text-5xl font-semibold">
                             Manage your users & weather in the simple way
                         </h1>
                         <p className="text-xl font-semibold">
